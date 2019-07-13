0df574b4aafbcf453740cd79fbc230b1cef65300
vladareanuv <52856257+vladareanuv@users.noreply.github.com>
2019-07-13 17:08:43 +0300

    Update eval_metrics.py

--- eval_metrics.py
+++ eval_metrics.py
@@ -15,3 +15,76 @@ def Rsqured(PA,GA):
     # 3 VARIABLES( categ: ce e comun, cat e detectat de noi, cat nu e detectat de noi)
     # FP nu au nici o area (pneumothorax)
     # classifiy about category: dupa ce rulam reteaua, sa clasificam imag in fct de cate zone de pneumotorax  
+
+
+
+
+def Tversky
+
+
+def Overlap
+
+
+def VOE
+
+
+def get_iou_vector(A, B):
+    # Numpy version    
+    batch_size = A.shape[0]
+    metric = 0.0
+    for batch in range(batch_size):
+        t, p = A[batch], B[batch]
+        true = np.sum(t)
+        pred = np.sum(p)
+        
+        # deal with empty mask first
+        if true == 0:
+            metric += (pred == 0)
+            continue
+        
+        # non empty mask case.  Union is never empty 
+        # hence it is safe to divide by its number of pixels
+        intersection = np.sum(t * p)
+        union = true + pred - intersection
+        iou = intersection / union
+        
+        # iou metrric is a stepwise approximation of the real iou over 0.5
+        iou = np.floor(max(0, (iou - 0.45)*20)) / 10
+        
+        metric += iou
+        
+    # teake the average over all images in batch
+    metric /= batch_size
+    return metric
+
+
+def my_iou_metric(label, pred):
+    # Tensorflow version
+    return tf.py_func(get_iou_vector, [label, pred > 0.5], tf.float64)
+
+def dice_coef(y_true, y_pred):
+    y_true_f = K.flatten(y_true)
+    y_pred = K.cast(y_pred, 'float32')
+    y_pred_f = K.cast(K.greater(K.flatten(y_pred), 0.5), 'float32')
+    intersection = y_true_f * y_pred_f
+    score = 2. * K.sum(intersection) / (K.sum(y_true_f) + K.sum(y_pred_f))
+    return score
+
+def dice_loss(y_true, y_pred):
+    smooth = 1.
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = y_true_f * y_pred_f
+    score = (2. * K.sum(intersection) + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+    return 1. - score
+
+def bce_dice_loss(y_true, y_pred):
+    return binary_crossentropy(y_true, y_pred) + dice_loss(y_true, y_pred)
+
+def bce_logdice_loss(y_true, y_pred):
+    return binary_crossentropy(y_true, y_pred) - K.log(1. - dice_loss(y_true, y_pred))
+
+def Hausdorff
+
+
+def A_noastra
